f3f4d69754a2fc31f55af66d1575dd0df13dbf57
oligamiq <nziq53@gmail.com>
2024-01-31 00:13:56 +0900

    これでいいはず！！

--- src/rng/arc4.rs
+++ src/rng/arc4.rs
@@ -1,27 +1,55 @@
-use std::pin::{pin, Pin};
+use std::{
+    marker::PhantomPinned,
+    pin::Pin,
+};
 
 use arc4::Arc4 as ImplArc4;
 
 use crate::rng_trait::{gen_seed_u8_32, u8_to_u64, RNG};
 
-pub struct Arc4 {
-  arc4: ImplArc4<'static>,
-  _seed: Pin<&'static mut [u8; 32]>,
+pub struct Arc4<'a> {
+    arc4: Option<ImplArc4<'a>>,
+    _seed: Pin<Box<SeedWrapper>>,
 }
 
-impl RNG for Arc4 {
-  fn new() -> Self {
-    let seed = gen_seed_u8_32();
-    let arc4: ImplArc4 = ImplArc4::with_key(&seed);
-    let seed_pin = pin!(seed);
-    Self {
-      arc4,
-      _seed: seed_pin,
+struct SeedWrapper {
+    seed: [u8; 32],
+    _pin: PhantomPinned,
+}
+
+impl SeedWrapper {
+    fn new(seed: [u8; 32]) -> Self {
+        Self {
+            seed,
+            _pin: PhantomPinned,
+        }
     }
-  }
-  fn get_random(&mut self) -> u64 {
-      let mut buff = [0u8; 8];
-      self.arc4.prga(&mut buff);
-      u8_to_u64(&buff)
-  }
+
+    unsafe fn get_seed_ref(&self) -> *const [u8; 32] {
+        &self.seed as *const [u8; 32]
+    }
+}
+
+impl<'a> RNG for Arc4<'a> {
+    fn new() -> Self {
+        let arc4 = {
+            let seed = gen_seed_u8_32();
+            Self {
+                arc4: None,
+                _seed: Box::pin(SeedWrapper::new(seed)),
+            }
+        };
+        let impl_arc4: ImplArc4 = ImplArc4::with_key(unsafe { &*arc4._seed.get_seed_ref() });
+        Self {
+            arc4: Some(impl_arc4),
+            _seed: arc4._seed,
+        }
+    }
+    fn get_random(&mut self) -> u64 {
+        let mut buff = [0u8; 8];
+        if let Some(ref mut arc4) = self.arc4 {
+            arc4.prga(buff.as_mut());
+        }
+        u8_to_u64(&buff)
+    }
 }
--- src/rng/chacha20.rs
+++ src/rng/chacha20.rs
@@ -1,13 +1,24 @@
-use rand_chacha::ChaCha20Rng as ImplChaCha20;
+use rand_chacha::{
+    rand_core::{RngCore as _, SeedableRng as _},
+    ChaCha20Rng as ImplChaCha20,
+};
 
-use crate::rng_trait::RNG;
+use crate::rng_trait::{gen_seed_u8_32, u8_to_u64, RNG};
 
 pub struct ChaCha20 {
-    chacha20: ImplChaCha20
+    chacha20: ImplChaCha20,
 }
 
 impl RNG for ChaCha20 {
     fn new() -> Self {
-      
+        Self {
+            chacha20: ImplChaCha20::from_seed(gen_seed_u8_32()),
+        }
+    }
+
+    fn get_random(&mut self) -> u64 {
+        let mut buff = [0u8; 8];
+        self.chacha20.fill_bytes(buff.as_mut());
+        u8_to_u64(&buff)
     }
 }
