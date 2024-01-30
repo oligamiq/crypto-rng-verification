5d7b74c87039c0080c7899af4ed037ab19d40125
oligamiq <nziq53@gmail.com>
2024-01-30 23:13:53 +0900

    これなら安全！！

--- src/rng/arc4.rs
+++ src/rng/arc4.rs
@@ -1,20 +1,22 @@
+use std::pin::{pin, Pin};
+
 use arc4::Arc4 as ImplArc4;
 
 use crate::rng_trait::{gen_seed_u8_32, u8_to_u64, RNG};
 
 pub struct Arc4 {
   arc4: ImplArc4<'static>,
-  _seed: [u8; 32],
+  _seed: Pin<&'static mut [u8; 32]>,
 }
 
 impl RNG for Arc4 {
   fn new() -> Self {
     let seed = gen_seed_u8_32();
-    let seed_p = &seed as *const [u8; 32];
-    let arc4: ImplArc4 = ImplArc4::with_key(unsafe { &*seed_p });
+    let arc4: ImplArc4 = ImplArc4::with_key(&seed);
+    let seed_pin = pin!(seed);
     Self {
       arc4,
-      _seed: seed,
+      _seed: seed_pin,
     }
   }
   fn get_random(&mut self) -> u64 {
